e32b68ca39b1a8b2e7093b68a17178ef7da59dd0
ffaure <frederic.faure@ext.cdiscount.com>
2018-12-03 16:18:12 +0100

    split if

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,31 +12,30 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE)
-                    || item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            } else if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn <= 10) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
-
-                            if (item.name.equals(BACKSTAGE_PASSES)) {
-                                if (item.sellIn <= 10) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn <= 5) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-                            }
                         }
-                    } else {
-                if (item.quality > 0) {
-                    if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                    } else {
-                        item.quality = item.quality - 1;
                     }
+
+                    if (item.sellIn <= 5) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            } else if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
 
