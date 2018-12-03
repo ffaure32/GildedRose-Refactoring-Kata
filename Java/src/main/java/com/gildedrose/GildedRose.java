a23a6912336b04486a8fb81c1e2ef0c80fc0c7e6
ffaure <frederic.faure@ext.cdiscount.com>
2018-12-03 16:22:14 +0100

    continue split if and replace if with switch

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,31 +12,36 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            switch (item.name) {
+                case AGED_BRIE:
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                    break;
+                case BACKSTAGE_PASSES:
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                    if (item.sellIn <= 10) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (item.sellIn <= 10) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn <= 5) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
                         }
                     }
-
-                    if (item.sellIn <= 5) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                    break;
+                case SULFURAS_HAND_OF_RAGNAROS:
+                    break;
+                default:
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
-                }
-            } else if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                    break;
             }
 
             if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
@@ -45,21 +50,22 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                } else {
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        item.quality = item.quality - item.quality;
-                    } else {
+                switch (item.name) {
+                    case AGED_BRIE:
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                        break;
+                    case BACKSTAGE_PASSES:
+                        item.quality = 0;
+                        break;
+                    case SULFURAS_HAND_OF_RAGNAROS:
+                        continue;
+                    default:
                         if (item.quality > 0) {
-                            if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                continue;
-                            }
                             item.quality = item.quality - 1;
                         }
-                    }
+                        break;
                 }
             }
         }
