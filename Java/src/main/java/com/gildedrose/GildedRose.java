98a82c2ea06726efabbd49b0e3d73231f8288989
ffaure <frederic.faure@ext.cdiscount.com>
2018-12-04 16:40:26 +0100

    invertif

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,51 +12,54 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASSES)) {
+            if (item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE_PASSES)) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+
+                            if (item.name.equals(BACKSTAGE_PASSES)) {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
+
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
+                            }
+                        }
+                    } else {
                 if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                    if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                    } else {
                         item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
                     }
                 }
             }
 
-            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+            } else {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.name.equals(AGED_BRIE)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        item.quality = item.quality - item.quality;
+                    } else {
+                        if (item.quality > 0) {
+                            if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+                                continue;
+                            }
+                            item.quality = item.quality - 1;
+                        }
                     }
                 }
             }
